38e5fc865e0bb5ccf81b8cbfd517eb5c1eeb1c03
gfffrtt <geogueserporque@gmail.com>
2025-01-27 20:07:00 -0300

    fix: remove unused import

--- src/infra/http/routes/test/commit_without_auth.ts
+++ src/infra/http/routes/test/commit_without_auth.ts
@@ -2,9 +2,6 @@ import { SPAN_STATUS_ERROR } from "@sentry/core";
 import { startSpan } from "@sentry/node";
 import { ambisisResponse, log, LogLevel } from "ambisis_node_helper";
 import type { Request, Response } from "express";
-import { getSyncProcessByUser } from "../../../../domain/process_sync/functions/db/get_sync_process_by_user";
-import { updateSyncProcess } from "../../../../domain/process_sync/functions/db/update_sync_process";
-import { ProcessSyncStatus } from "../../../../domain/process_sync/types/sync_process";
 import { getSyncProcessTransactionByUserId } from "../../../../usecases/changes/functions/get_sync_process_transaction_by_user_id";
 
 export const commit = (req: Request, res: Response) =>
