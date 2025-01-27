3bd637efacf637c5a7f506e79067810c8639dc1c
gfffrtt <geogueserporque@gmail.com>
2025-01-27 20:41:12 -0300

    fix: remove default from zod

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -2,21 +2,19 @@ import { z } from "zod";
 import "dotenv/config";
 
 export const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
-  PUBLIC_KEY: z.string().default("12345"),
-  AUTH_SERVICE_URL: z.string().default("http://auth_service:3031"),
-  AUTH_SERVICE_PUBLIC_KEY: z.string().default("12345"),
-  AUTH_SERVICE_PRIVATE_KEY: z.string().default("12345"),
+  NODE_ENV: z.enum(["development", "production", "test"]),
+  PUBLIC_KEY: z.string(),
+  AUTH_SERVICE_URL: z.string(),
+  AUTH_SERVICE_PUBLIC_KEY: z.string(),
+  AUTH_SERVICE_PRIVATE_KEY: z.string(),
 
-  PRIVATE_SERVICE_URL: z.string().default("http://private_services:3036"),
-  PRIVATE_SERVICE_PRIVATE_KEY: z.string().default("12345"),
+  PRIVATE_SERVICE_URL: z.string(),
+  PRIVATE_SERVICE_PRIVATE_KEY: z.string(),
 
-  DATABASE_HOST: z.string().default("mysql_local"),
-  DATABASE_PORT: z.coerce.number().default(3306),
-  DATABASE_USER: z.string().default("root"),
-  DATABASE_PASSWORD: z.string().default(""),
+  DATABASE_HOST: z.string(),
+  DATABASE_PORT: z.coerce.number(),
+  DATABASE_USER: z.string(),
+  DATABASE_PASSWORD: z.string(),
 
   AWS_S3_REGION: z.string(),
   AWS_S3_KEY: z.string(),
