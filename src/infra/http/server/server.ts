a1715710c249efefb4ce2e17984d18f9232fc561
gfffrtt <geogueserporque@gmail.com>
2025-01-27 11:46:56 -0300

    feat: add health check

--- src/infra/http/server/server.ts
+++ src/infra/http/server/server.ts
@@ -1,8 +1,11 @@
 import type { Application } from "express";
 import { middleware } from "../middleware";
 import { router } from "../routes";
+import { healthCheckRoute } from "../routes/health_check";
 
 export const server = (app: Application) => {
+  healthCheckRoute(app);
+
   middleware(app);
 
   router(app);
