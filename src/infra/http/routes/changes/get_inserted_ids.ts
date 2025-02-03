adf2c0f474576e7b7d4f40d7e148cf9a0214b01b
gfffrtt <geogueserporque@gmail.com>
2025-02-03 10:54:51 -0300

    feat: aaaaa close

--- src/infra/http/routes/changes/get_inserted_ids.ts
+++ src/infra/http/routes/changes/get_inserted_ids.ts
@@ -1,6 +1,7 @@
 import type { Application } from "express";
 import { CHANGES } from "../endpoints.json";
+import { getInsertedIds } from "../../../../usecases/changes/get_inserted_ids/get_inserted_ids";
 
 export const getInsertedIdsRoute = (app: Application) => {
-  app.post(CHANGES.GET_INSERTED_IDS, getInsertedIdsRoute);
+  app.post(CHANGES.GET_INSERTED_IDS, getInsertedIds);
 };
