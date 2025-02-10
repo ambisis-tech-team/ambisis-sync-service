61c105053664c3eb60aabcebd4c050656a1ff253
gfffrtt <geogueserporque@gmail.com>
2025-02-10 15:09:36 -0300

    feat: ajustments to route

--- src/usecases/files/files.ts
+++ src/usecases/files/files.ts
@@ -7,7 +7,7 @@ import { s3Client } from "../../infra/s3/client";
 import { Upload } from "@aws-sdk/lib-storage";
 import { env } from "../../infra/env/env";
 import { getFileById } from "../../domain/file/functions/get_file_by_id";
-import { EventEmitter } from "stream";
+import internal, { EventEmitter } from "stream";
 import { updateFile } from "../../domain/file/functions/update_file";
 import { db } from "../../infra/db/db";
 import { FileIsSynced } from "../../domain/file/types/file";
@@ -16,15 +16,30 @@ import { Time } from "../../shared/types/time";
 export const files = (req: Request, res: Response) =>
   startSpan({ name: "file" }, async (span) => {
     const { database, user_id } = req.session;
+    const abortController = new AbortController();
+
+    const abortTimeout = setTimeout(
+      () => abortController.abort(),
+      Time.MINUTE * 5
+    );
+
     try {
       log(
         `Starting files sync - userId: ${user_id} - database: ${database}`,
         LogLevel.INFO
       );
 
-      const abortController = new AbortController();
+      req.on("close", () => {
+        if (abortController.signal.aborted) return;
+        log(
+          `Request closed prematurely - userId: ${user_id} - database: ${database}`,
+          LogLevel.INFO
+        );
+        abortController.abort("Client Disconnect");
+      });
 
       abortController.signal.addEventListener("abort", () => {
+        clearTimeout(abortTimeout);
         if (res.writableEnded) return;
         log(
           `Files sync aborted - userId: ${user_id} - database: ${database}`,
@@ -35,107 +50,103 @@ export const files = (req: Request, res: Response) =>
         res.end();
       });
 
-      setTimeout(() => abortController.abort(), Time.MINUTE * 5);
-
-      const bb = busboy({ headers: req.headers });
+      const bb = busboy({
+        headers: req.headers,
+        limits: { fields: 1, files: 5 },
+      });
 
       const failedSuccessfulFiles: {
         successUploadedFiles: Array<number>;
         failedUploadedFiles: Array<number>;
       } = { failedUploadedFiles: [], successUploadedFiles: [] };
       let totalFiles: number = 0;
-
-      const uploads = new EventEmitter();
-
-      uploads.addListener("upload", () => {
-        if (res.writableEnded) return;
-        if (
-          !totalFiles ||
-          failedSuccessfulFiles.successUploadedFiles.length +
-            failedSuccessfulFiles.failedUploadedFiles.length !==
-            totalFiles
-        )
-          return;
-        log(
-          `Finished files sync - userId: ${user_id} - database: ${database}`,
-          LogLevel.INFO
-        );
-        span.setStatus({ code: SPAN_STATUS_OK, message: "Files synced" });
-        ambisisResponse(res, 200, "SUCCESS", failedSuccessfulFiles);
-        res.end();
-      });
+      let processedFiles: number = 0;
 
       bb.on("field", (fieldName, value) => {
         if (fieldName === "totalFiles") totalFiles = Number(value);
       });
 
-      bb.on("file", (fieldName, body, info) => {
+      const processFile = async (
+        fieldName: string,
+        body: internal.Readable,
+        info: busboy.FileInfo
+      ) => {
+        if (res.writableEnded || abortController.signal.aborted) {
+          body.resume();
+          return;
+        }
+
         const { mimeType } = info;
         const fileId = Math.abs(Number(fieldName));
 
-        getFileById(fileId, database)
-          .then((file) => {
-            if (file.s3FileStatus === FileIsSynced.SYNCED) {
-              failedSuccessfulFiles.successUploadedFiles.push(fileId);
-              uploads.emit("upload");
-              return;
-            }
+        try {
+          const file = await getFileById(fileId, database);
 
-            const upload = new Upload({
-              client: s3Client,
-              params: {
-                Bucket: env.AWS_S3_BUCKET,
-                Key: file.keyS3,
-                Body: body,
-                ContentType: mimeType,
-              },
-            });
+          if (file.s3FileStatus === FileIsSynced.SYNCED) {
+            failedSuccessfulFiles.successUploadedFiles.push(fileId);
+            return;
+          }
 
-            upload
-              .done()
-              .then(() => {
-                log(
-                  `File uploaded - fileId: ${fileId} - userId: ${user_id} - database: ${database}`
-                );
-                updateFile(
-                  db,
-                  {
-                    id: fileId,
-                    s3FileStatus: FileIsSynced.SYNCED,
-                    modificacaoData: new Date(),
-                  },
-                  database
-                )
-                  .then(() => {
-                    failedSuccessfulFiles.successUploadedFiles.push(fileId);
-                    uploads.emit("upload");
-                  })
-                  .catch((err) => {
-                    log(
-                      `Error setting file as synced - ${fileId} - ${user_id} - ${database}: ${err.message}`,
-                      LogLevel.ERROR
-                    );
-                    failedSuccessfulFiles.failedUploadedFiles.push(fileId);
-                    uploads.emit("upload");
-                  });
-              })
-              .catch((err) => {
-                log(
-                  `Error uploading file - ${fileId} - ${user_id} - ${database}: ${err.message}`,
-                  LogLevel.ERROR
-                );
-                failedSuccessfulFiles.failedUploadedFiles.push(fileId);
-                uploads.emit("upload");
-              });
-          })
-          .catch((err) => {
-            log(
-              `Error fetching file by ID - ${fileId} - ${user_id} - ${database}: ${err.message}`,
-              LogLevel.ERROR
-            );
-            failedSuccessfulFiles.failedUploadedFiles.push(fileId);
-            uploads.emit("upload");
+          const upload = new Upload({
+            client: s3Client,
+            params: {
+              Bucket: env.AWS_S3_BUCKET,
+              Key: file.keyS3,
+              Body: body,
+              ContentType: mimeType,
+            },
+            partSize: 5 * 1024 * 1024,
+            leavePartsOnError: false,
           });
+
+          await upload.done();
+
+          log(
+            `File uploaded - fileId: ${fileId} - userId: ${user_id} - database: ${database}`
+          );
+
+          await updateFile(
+            db,
+            {
+              id: fileId,
+              s3FileStatus: FileIsSynced.SYNCED,
+              modificacaoData: new Date(),
+            },
+            database
+          );
+
+          failedSuccessfulFiles.successUploadedFiles.push(fileId);
+        } catch (error) {
+          log(
+            `Error fetching file by ID - ${fileId} - ${user_id} - ${database}: ${error}`,
+            LogLevel.ERROR
+          );
+          failedSuccessfulFiles.failedUploadedFiles.push(fileId);
+        } finally {
+          body.resume();
+          processedFiles++;
+          if (
+            processedFiles !== totalFiles ||
+            res.writableEnded ||
+            abortController.signal.aborted
+          )
+            return;
+          clearTimeout(abortTimeout);
+          log(
+            `Finished files sync - userId: ${user_id} - database: ${database}`,
+            LogLevel.INFO
+          );
+          span.setStatus({
+            code: SPAN_STATUS_OK,
+            message: "Files synced",
+          });
+          ambisisResponse(res, 200, "SUCCESS", failedSuccessfulFiles);
+          res.end();
+        }
+      };
+
+      bb.on("file", (fieldName, body, info) => {
+        processFile(fieldName, body, info);
       });
 
       req.pipe(bb);
